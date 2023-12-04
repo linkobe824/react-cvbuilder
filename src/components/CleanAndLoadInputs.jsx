17725bed74b9cb81cdbe7021de8b160d3d765467
Manuel Beltran <manueldbeltranc@gmail.com>
2023-12-03 23:35:12 -0700

    add CleanAndLoadInputs component and style

--- src/components/CleanAndLoadInputs.jsx
+++ src/components/CleanAndLoadInputs.jsx
@@ -1,8 +1,12 @@
+import classes from '../css/CleanAndLoadInputs.module.css'
+
 function CleanAndLoadInputs() {
   return (
     <div className='container'>
-      <button>Clear Resume</button>
-      <button>Load Example Data</button>
+      <div className={classes['button-container']}>
+        <button className={classes['clear-button']}>Clear Resume</button>
+        <button className={classes['load-button']}>Load Example Data</button>
+      </div>
     </div>
   )
 }
